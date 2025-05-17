629401e1298800dd6ea0a6105a8cf0485ef74725
Eric Chen <ericchen098@gmail.com>
2025-05-16 18:10:17 -0700

    add `multiply.ts` and use it in project2

--- project2/src/main.ts
+++ project2/src/main.ts
@@ -1,5 +1,7 @@
 import {add} from 'project1/add';
 import {subtract} from 'project1/subtract';
+import {multiply} from 'project1/multiply';
 
 console.log('add', add(1, 2));
 console.log('subtract', subtract(1, 2));
+console.log('multiply', multiply(1, 2));
